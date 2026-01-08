eebd190432c3bd0796f535bb9932602ad36d3620
project516 <138796702+Project516@users.noreply.github.com>
2026-01-08 15:44:51 +0000

    move code into classes

---
+++ app/src/main/java/dev/project516/CurrentTemperature/Location.java
@@ -0,0 +1,52 @@
+package dev.project516.CurrentTemperature;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Location {
+
+    double lat;
+    double lon;
+    String city;
+
+    public Location() {
+        try {
+
+            HttpClient client = HttpClient.newHttpClient();
+            ObjectMapper mapper = new ObjectMapper();
+
+            HttpRequest locationRequest =
+                    HttpRequest.newBuilder()
+                            .uri(URI.create("http://ip-api.com/json/"))
+                            .GET()
+                            .build();
+
+            HttpResponse<String> locactionResponse =
+                    client.send(locationRequest, HttpResponse.BodyHandlers.ofString());
+
+            JsonNode locationNode = mapper.readTree(locactionResponse.body());
+
+            lat = locationNode.get("lat").asDouble();
+            lon = locationNode.get("lon").asDouble();
+            city = locationNode.get("city").asText();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    double getLat() {
+        return lat;
+    }
+
+    double getlon() {
+        return lon;
+    }
+
+    String getCity() {
+        return city;
+    }
+}
